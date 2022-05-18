e4847e8388b9c8516a9838ef060c54f0ed1d1557
kaiiy <kaiiy@kaiix.tokyo>
2022-05-18 20:08:08 +0900

    feat: add init fish, neovim

--- init.sh
+++ init.sh
@@ -10,3 +10,30 @@ init_rust() {
 	cargo install cargo-update
 }
 
+init_fish() {
+	echo Start fish shell installation!
+	
+	sudo apt-add-repository ppa:fish-shell/release-3
+	sudo apt-get update
+	sudo apt-get upgrade -y
+	sudo apt-get install fish
+
+	mkdir -p $HOME/.config/fish/conf.d/
+	mkdir -p $HOME/.config/fish/functions/
+
+	fish
+	curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish\
+		| source && fisher install jorgebucaran/fisher
+}
+
+init_neovim() {
+	echo Start neovim installation!
+
+	sudo add-apt-repository ppa:neovim-ppa/stable
+	sudo apt-get update
+	sudo apt-get upgrade -y
+
+	# todo: add package installtion feat  
+}
+
+
