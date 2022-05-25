21f59cb7b74808823a71f333fbcb8860d8af81c3
kaiiy <kaiiy@kaiix.tokyo>
2022-05-25 13:06:08 +0900

    fix: not overwritten when installing fisher

--- link_config.sh
+++ link_config.sh
@@ -3,17 +3,18 @@
 PWD=$(pwd)
 
 # fish
-ln -sf $PWD/conf/fish/config.fish -t ~/.config/fish/
-ln -sf $PWD/conf/fish/fish_plugins -t ~/.config/fish/
-ln -sf $PWD/conf/fish/conf.d/color_scheme.fish -t ~/.config/fish/conf.d/
-find $PWD/conf/fish/functions/ -maxdepth 1 -type f -print | xargs ln -sf -t ~/.config/fish/functions/ 
+ln -sf $PWD/conf/fish/config.fish -t $HOME/.config/fish/
+unlink $HOME/.config/fish/fish_plugins > /dev/null 2>&1
+cp -f $PWD/conf/fish/fish_plugins $HOME/.config/fish/
+ln -sf $PWD/conf/fish/conf.d/color_scheme.fish -t $HOME/.config/fish/conf.d/
+find $PWD/conf/fish/functions/ -maxdepth 1 -type f -print | xargs ln -sf -t $HOME/.config/fish/functions/ 
 
 # lftp 
-ln -sf $PWD/conf/lftp/lftprc ~/.lftprc
+ln -sf $PWD/conf/lftp/lftprc $HOME/.lftprc
 
 # nvim 
-ln -sf $PWD/conf/nvim/init.vim -t ~/.config/nvim/
-find $PWD/conf/nvim/configs/ -maxdepth 1 -type f -print | xargs ln -sf -t ~/.config/nvim/configs/ 
+ln -sf $PWD/conf/nvim/init.vim -t $HOME/.config/nvim/
+find $PWD/conf/nvim/configs/ -maxdepth 1 -type f -print | xargs ln -sf -t $HOME/.config/nvim/configs/ 
 
 # bash
-ln -sf $PWD/conf/bash/bashrc ~/.bashrc 
+ln -sf $PWD/conf/bash/bashrc $HOME/.bashrc 
