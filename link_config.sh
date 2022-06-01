f97d061daeb7499b509eaac7e4387b7ba602e022
kaiiy <kaiiy@kaiix.tokyo>
2022-06-01 22:55:44 +0900

    refactor: add ln_dir func

--- link_config.sh
+++ link_config.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
+ln_dir () {
+    find $1 -maxdepth 1 -type f -print | xargs ln -sf -t $2
+}
+
 PWD=$(pwd)
+
+# bash
+ln -sf $PWD/conf/bash/bashrc $HOME/.bashrc 
 
 # fish
 ln -sf $PWD/conf/fish/config.fish -t $HOME/.config/fish/
 unlink $HOME/.config/fish/fish_plugins > /dev/null 2>&1
 cp -f $PWD/conf/fish/fish_plugins $HOME/.config/fish/
 ln -sf $PWD/conf/fish/conf.d/color_scheme.fish -t $HOME/.config/fish/conf.d/
-find $PWD/conf/fish/functions/ -maxdepth 1 -type f -print | xargs ln -sf -t $HOME/.config/fish/functions/ 
+ln_dir $PWD/conf/fish/functions/ $HOME/.config/fish/functions/ 
 
 # lftp 
 ln -sf $PWD/conf/lftp/lftprc $HOME/.lftprc
 
 # nvim 
 ln -sf $PWD/conf/nvim/init.vim -t $HOME/.config/nvim/
-find $PWD/conf/nvim/configs/ -maxdepth 1 -type f -print | xargs ln -sf -t $HOME/.config/nvim/configs/ 
-
-# bash
-ln -sf $PWD/conf/bash/bashrc $HOME/.bashrc 
+ln_dir $PWD/conf/nvim/configs/ $HOME/.config/nvim/configs/ 
