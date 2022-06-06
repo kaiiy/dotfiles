8ece00059b9f976d1cb90fb7430cca9b63f5c931
kaiiy <kaiiy@kaiix.tokyo>
2022-06-06 21:40:30 +0900

    fix: vim-plug

--- init.sh
+++ init.sh
@@ -37,6 +37,10 @@ init_neovim() {
 	sudo apt-get update
 	sudo apt-get upgrade -y
 
+	# install vim-plug 
+	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 	# todo: add package installtion feat  
 }
 
