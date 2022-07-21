e4fc6d3cc45ae06319221479511274976e21cbcc
kaiiy <kaiiy@kaiix.tokyo>
2022-07-21 17:30:49 +0900

    feat: add pdfmin

--- conf/fish/functions/pdfmin.fish
+++ conf/fish/functions/pdfmin.fish
@@ -1,3 +1,6 @@
 function pdfmin
-    
+    set input_file_path (string join '' (pwd) / $argv[1])
+    set output_file_path (string replace .pdf .min.pdf $input_file_path)
+
+    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$output_file_path $input_file_path
 end
