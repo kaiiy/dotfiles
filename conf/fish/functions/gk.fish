e394c95bbd2c79563c91c0866dde477ab355542b
kaiiy <kaiiy@kaiix.tokyo>
2022-07-21 17:03:35 +0900

    feat: change a commit message

--- conf/fish/functions/gk.fish
+++ conf/fish/functions/gk.fish
@@ -2,7 +2,7 @@
 function gk
     if string match '*Lab*' $(pwd) >/dev/null 2>&1
         git add .
-        if git commit -m (string join ''  "save: " $(date --rfc-3339='seconds'))
+        if git commit -m (string join ''  "wip: " $(date --rfc-3339='seconds'))
             git push
         end
     end
