dcd827b9d6b2107b0cfff27b33ae4171b626cda2
kaiiy <kaiiy@kaiix.tokyo>
2022-08-18 15:05:04 +0900

    refactor

--- init.sh
+++ init.sh
@@ -1,25 +1,13 @@
 #!/usr/bin/env bash
 
 init_ubuntu() {
-	sudo apt-get install pkg-config
-}
-
-init_node() {
-	curl https://get.volta.sh | bash -s -- --skip-setup
-	volta install node
-	volta install yarn
-}
-
-init_rust() {
-	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-	cargo install bat cargo-update exa fd-find procs ripgrep topgrade zellij deno
+	sudo apt-get install pkg-config curl build-essential gcc make -y
 }
 
 init_fish() {
 	sudo apt-add-repository ppa:fish-shell/release-3
 	sudo apt-get update
-	sudo apt-get upgrade -y
-	sudo apt-get install fish
+	sudo apt-get install fish -y
 
 	mkdir -p $HOME/.config/fish/conf.d/
 	mkdir -p $HOME/.config/fish/functions/
@@ -29,10 +17,22 @@ init_fish() {
 		| source && fisher install jorgebucaran/fisher
 }
 
+init_node() {
+	curl https://get.volta.sh | bash -s -- --skip-setup
+	volta install node
+	volta install yarn
+}
+
+init_rust() {
+	curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh
+	fish
+	cargo install bat cargo-update exa fd-find procs ripgrep topgrade zellij deno
+}
+
 init_neovim() {
 	sudo add-apt-repository ppa:neovim-ppa/stable
 	sudo apt-get update
-	sudo apt-get upgrade -y
+	sudo apt-get install neovim -y
 
 	# install vim-plug 
 	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
