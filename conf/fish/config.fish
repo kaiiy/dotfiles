f9786c1be1a288a8a104671ab56f5bc13db8edba
kaiiy <81578246+kaiiy@users.noreply.github.com>
2022-09-18 19:25:17 +0900

    feat: add poetry abbr

--- conf/fish/config.fish
+++ conf/fish/config.fish
@@ -44,6 +44,7 @@ if status is-interactive
 	# python 
 	# alias python3="/home/linuxbrew/.linuxbrew/bin/python3.10"
 	# alias pip3="/home/linuxbrew/.linuxbrew/bin/pip3.10"
+	abbr -a poe poetry
 
     # set fish_cursor_default line
     # set fish_cursor_insert line
