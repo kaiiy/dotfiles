b8bbc15b3bb1f252c19bae5976f0f29815f6972f
kaiiy <81578246+kaiiy@users.noreply.github.com>
2022-10-14 15:44:12 +0900

    fix: typo

--- link.sh
+++ link.sh
@@ -6,19 +6,19 @@ ln_dir () {
 
 PWD=$(pwd)
 SRC_HOME=$PWD/conf
-DIST_HOME=$HOME/.config
+DEST_HOME=$HOME/.config
 
 # bash
 ln -sf $SRC_HOME/bash/bashrc $HOME/.bashrc 
 
 # fish
 FISH_SRC_HOME=$SRC_HOME/fish
-FISH_DIST_HOME=$DIST_HOME/fish
+FISH_DEST_HOME=$DEST_HOME/fish
 
-ln -sf $FISH_SRC_HOME/config.fish -t $FISH_DIST_HOME/
-cp -f $FISH_SRC_HOME/fish_plugins $FISH_DIST_HOME/
-ln_dir $FISH_SRC_HOME/conf.d/color_scheme.fish $FISH_DIST_HOME/conf.d/
-ln_dir $FISH_SRC_HOME/functions/ $FISH_DIST_HOME/functions/ 
+ln -sf $FISH_SRC_HOME/config.fish -t $FISH_DEST_HOME/
+cp -f $FISH_SRC_HOME/fish_plugins $FISH_DEST_HOME/
+ln_dir $FISH_SRC_HOME/conf.d/color_scheme.fish $FISH_DEST_HOME/conf.d/
+ln_dir $FISH_SRC_HOME/functions/ $FISH_DEST_HOME/functions/ 
 
 # lftp 
 ln -sf $SRC_HOME/lftp/lftprc $HOME/.lftprc
@@ -26,23 +26,23 @@ ln -sf $SRC_HOME/lftp/lftprc $HOME/.lftprc
 # nvim 
 # migrating to AstroNvim (https://github.com/AstroNvim/AstroNvim)
 # NVIM_SRC_HOME=$SRC_HOME/nvim
-# NVIM_DIST_HOME=$DIST_HOME/nvim
+# NVIM_DEST_HOME=$DEST_HOME/nvim
 
-# mkdir -p $NVIM_DIST_HOME/configs/
-# ln -sf $NVIM_SRC_HOME/init.vim -t $NVIM_DIST_HOME/
-# ln_dir $NVIM_SRC_HOME/configs/ $NVIM_DIST_HOME/configs/ 
+# mkdir -p $NVIM_DEST_HOME/configs/
+# ln -sf $NVIM_SRC_HOME/init.vim -t $NVIM_DEST_HOME/
+# ln_dir $NVIM_SRC_HOME/configs/ $NVIM_DEST_HOME/configs/ 
 
 # topgrade
 TOPGRADE_SRC=$SRC_HOME/topgrade
-TOPGRADE_DIST=$DIST_HOME
-ln -sf $TOPGRADE_SRC/topgrade.toml $TOPGRADE_DIST
+TOPGRADE_DEST=$DEST_HOME
+ln -sf $TOPGRADE_SRC/topgrade.toml $TOPGRADE_DEST
 
 # zellij
 ZELLIJ_SRC_HOME=$SRC_HOME/zellij
-ZELLIJ_DIST_HOME=$DIST_HOME/zellij
+ZELLIJ_DEST_HOME=$DEST_HOME/zellij
 
-mkdir -p $ZELLIJ_DIST_HOME/
-ln -sf $ZELLIJ_SRC_HOME/config.yaml -t $ZELLIJ_DIST_HOME/
+mkdir -p $ZELLIJ_DEST_HOME/
+ln -sf $ZELLIJ_SRC_HOME/config.yaml -t $ZELLIJ_DEST_HOME/
 
 # task
 if [ ! -f ~/.config/fish/completions/task.fish ]; then
@@ -52,6 +52,6 @@ fi
 
 # bat
 BAT_SRC=$SRC_HOME/bat
-BAT_DEST=$DIST_HOME/bat
+BAT_DEST=$DEST_HOME/bat
 mkdir -p $BAT_DEST/
 ln -sf $BAT_SRC/config -t $BAT_DEST
