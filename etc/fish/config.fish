2051d4d26c388afc5e1ccb4b673c03f04a8985e0
kaiiy <81578246+kaiiy@users.noreply.github.com>
2022-11-17 13:11:44 +0900

    perf: inkscape

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -62,7 +62,7 @@ if status is-interactive
 	fish_add_path $HOME/go/bin
 
     # env 
-    set -x DISPLAY $(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0.0
+    # set -x DISPLAY $(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0.0
 
     eval "$(zellij setup --generate-auto-start fish)"
 end
