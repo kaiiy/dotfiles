f1d831c8aa75729bc23098b76a984f26a993aa31
kaiiy <81578246+kaiiy@users.noreply.github.com>
2022-12-19 15:12:55 +0900

    feat(fish): mkpass

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -64,6 +64,9 @@ if status is-interactive
     # wsl2
     alias clip="clip.exe"
 
+    # generate a password
+    abbr -a mkpass pwgen -cnsB 12
+
     # path 
     fish_add_path $HOME/.cargo/bin
     fish_add_path $HOME/.local/bin
