ce07942160644c7b84e67bb24a2ef9ed73b442c1
kaiiy <81578246+kaiiy@users.noreply.github.com>
2023-02-06 00:27:18 +0900

    feat: add scripts for installation

--- bin/init/docker.sh
+++ bin/init/docker.sh
@@ -14,3 +14,6 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo service docker stop
+sudo service docker start
