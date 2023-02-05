9948462c4089a6c134cd08a5f71aaecb9614bdd6
kaiiy <81578246+kaiiy@users.noreply.github.com>
2023-02-06 00:33:22 +0900

    feat: add topgrade & astronvim

---
+++ bin/init/astronvim.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+nvim +PackerSync
--- bin/init/homebrew.sh
+++ bin/init/homebrew.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 brew tap tgotwig/linux-dust
-brew install deno zellij bat dust exa fd duf procs bottom ripgrep trash-cli neovim
+brew install deno zellij bat dust exa fd duf procs bottom ripgrep trash-cli neovim topgrade
