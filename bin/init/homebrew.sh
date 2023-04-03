e4a16763dfe134fa7d0fed216e397396e3eb8108
kaiiy <81578246+kaiiy@users.noreply.github.com>
2023-04-03 11:23:22 +0900

    feat: rustscan

--- bin/init/homebrew.sh
+++ bin/init/homebrew.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 brew tap tgotwig/linux-dust
-brew install deno zellij bat dust exa fd duf procs bottom ripgrep trash-cli neovim topgrade go-task/tap/go-task
+brew install \
+  deno zellij bat dust exa fd duf procs bottom ripgrep \
+  trash-cli neovim topgrade go-task/tap/go-task rustscan
