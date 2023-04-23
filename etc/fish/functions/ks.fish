b1b483a812faf6a3e747beaeb257dadd7978e822
kaiiy <81578246+kaiiy@users.noreply.github.com>
2023-04-23 22:18:03 +0900

    feat: add comments when executing `ks`

--- etc/fish/functions/ks.fish
+++ etc/fish/functions/ks.fish
@@ -1,5 +1,10 @@
 function ks
+    set_color green; echo "[z]"; set_color normal
     z -c
+    set_color green; echo "[Docker Image]"; set_color normal
     docker image prune -af 
-    rm -rf ~/.local/share/Trash/
+    set_color green; echo "[.local/share/Trash]"; set_color normal
+    mkdir -p ~/.local/share/Trash
+    du -sh ~/.local/share/Trash/
+    rm -rfv ~/.local/share/Trash/ | grep --color=never 'directory'
 end
