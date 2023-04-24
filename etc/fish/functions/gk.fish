e8ad6065285e07149e674271b1885a97c6a8b0f9
kaiiy <81578246+kaiiy@users.noreply.github.com>
2023-04-24 19:05:02 +0900

    perf

--- etc/fish/functions/gk.fish
+++ etc/fish/functions/gk.fish
@@ -1,6 +1,8 @@
 function gk
+    set_color green; echo "[git add]"; set_color normal
     git add .
     if git commit -m (string join ''  "wip: " $(date --rfc-3339='seconds'))
+        set_color green; echo "[git push]"; set_color normal
         git push
     end
 end
