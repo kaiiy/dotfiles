c7281c6b4b6ae14aba5c787a3add8aee6ea8373a
kaiiy <81578246+kaiiy@users.noreply.github.com>
2023-06-20 11:20:15 +0900

    style

--- link.sh
+++ link.sh
@@ -8,72 +8,72 @@ cp_link() {
 }
 
 PWD=$(pwd)
-SRC_HOME=$PWD/etc
-DEST_HOME=$HOME/.config
+SRC=$PWD/etc
+DEST=$HOME/.config
 
 # bash
-ln -sf $SRC_HOME/bash/bashrc $HOME/.bashrc
+ln -sf $SRC/bash/bashrc $HOME/.bashrc
 
 # fish
-FISH_SRC_HOME=$SRC_HOME/fish
-FISH_DEST_HOME=$DEST_HOME/fish
+FISH_SRC=$SRC/fish
+FISH_DEST=$DEST/fish
 
-mkdir -p $FISH_DEST_HOME/conf.d/
-mkdir -p $FISH_DEST_HOME/functions/
-mkdir -p $FISH_DEST_HOME/completions/
+mkdir -p $FISH_DEST/conf.d/
+mkdir -p $FISH_DEST/functions/
+mkdir -p $FISH_DEST/completions/
 
-ln -sf $FISH_SRC_HOME/config.fish -t $FISH_DEST_HOME/
-cp -f $FISH_SRC_HOME/fish_plugins $FISH_DEST_HOME/
-ln_dir $FISH_SRC_HOME/conf.d/color_scheme.fish $FISH_DEST_HOME/conf.d/
-ln_dir $FISH_SRC_HOME/functions/ $FISH_DEST_HOME/functions/
-cp_link /home/linuxbrew/.linuxbrew/share/fish/vendor_completions.d/ $FISH_DEST_HOME/completions/
+ln -sf $FISH_SRC/config.fish -t $FISH_DEST/
+cp -f $FISH_SRC/fish_plugins $FISH_DEST/
+ln_dir $FISH_SRC/conf.d/color_scheme.fish $FISH_DEST/conf.d/
+ln_dir $FISH_SRC/functions/ $FISH_DEST/functions/
+cp_link /home/linuxbrew/.linuxbrew/share/fish/vendor_completions.d/ $FISH_DEST/completions/
 
 # task completion
-wget -O $FISH_DEST_HOME/completions/task.fish https://raw.githubusercontent.com/kaiiy/task-completion-for-fish/main/task.fish >>/dev/null 2>&1
+wget -O $FISH_DEST/completions/task.fish https://raw.githubusercontent.com/kaiiy/task-completion-for-fish/main/task.fish >>/dev/null 2>&1
 
 # lftp
-ln -sf $SRC_HOME/lftp/lftprc $HOME/.lftprc
+ln -sf $SRC/lftp/lftprc $HOME/.lftprc
 
 # nvim
 # migrating to AstroNvim (https://github.com/AstroNvim/AstroNvim)
-# NVIM_SRC_HOME=$SRC_HOME/nvim
-# NVIM_DEST_HOME=$DEST_HOME/nvim
+# NVIM_SRC=$SRC/nvim
+# NVIM_DEST=$DEST/nvim
 
-# mkdir -p $NVIM_DEST_HOME/configs/
-# ln -sf $NVIM_SRC_HOME/init.vim -t $NVIM_DEST_HOME/
-# ln_dir $NVIM_SRC_HOME/configs/ $NVIM_DEST_HOME/configs/
+# mkdir -p $NVIM_DEST/configs/
+# ln -sf $NVIM_SRC/init.vim -t $NVIM_DEST/
+# ln_dir $NVIM_SRC/configs/ $NVIM_DEST/configs/
 
 # topgrade
-TOPGRADE_SRC=$SRC_HOME/topgrade
-TOPGRADE_DEST=$DEST_HOME
+TOPGRADE_SRC=$SRC/topgrade
+TOPGRADE_DEST=$DEST
 ln -sf $TOPGRADE_SRC/topgrade.toml $TOPGRADE_DEST
 
 # zellij
-ZELLIJ_SRC_HOME=$SRC_HOME/zellij
-ZELLIJ_DEST_HOME=$DEST_HOME/zellij
+ZELLIJ_SRC=$SRC/zellij
+ZELLIJ_DEST=$DEST/zellij
 
-mkdir -p $ZELLIJ_DEST_HOME/
-ln -sf $ZELLIJ_SRC_HOME/config.kdl -t $ZELLIJ_DEST_HOME/
+mkdir -p $ZELLIJ_DEST/
+ln -sf $ZELLIJ_SRC/config.kdl -t $ZELLIJ_DEST/
 
 # bat
-BAT_SRC=$SRC_HOME/bat
-BAT_DEST=$DEST_HOME/bat
+BAT_SRC=$SRC/bat
+BAT_DEST=$DEST/bat
 
 mkdir -p $BAT_DEST/
 ln -sf $BAT_SRC/config -t $BAT_DEST
 
 # inkscape
-# INKSCAPE_SRC=$SRC_HOME/inkscape
-# INKSCAPE_DEST=$DEST_HOME/inkscape
-# mkdir -p $DEST_HOME/inkscape/
+# INKSCAPE_SRC=$SRC/inkscape
+# INKSCAPE_DEST=$DEST/inkscape
+# mkdir -p $DEST/inkscape/
 # ln -sf $INKSCAPE_SRC/preferences.xml -t $INKSCAPE_DEST
 
 # hyper
-HYPER_SRC=$SRC_HOME/hyper
+HYPER_SRC=$SRC/hyper
 HYPER_DEST=$HOME
 ln -sf $HYPER_SRC/hyper.js $HYPER_DEST/.hyper.js
 
 # rustscan
-HYPER_SRC=$SRC_HOME/rustscan
+HYPER_SRC=$SRC/rustscan
 HYPER_DEST=$HOME
 ln -sf $HYPER_SRC/rustscan.toml $HYPER_DEST/.rustscan.toml
