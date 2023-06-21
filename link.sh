aa3e453957275c3f82ae338e1349f5077d0eb55a
kaiiy <81578246+kaiiy@users.noreply.github.com>
2023-06-21 13:58:49 +0900

    fix(zellij)

--- link.sh
+++ link.sh
@@ -52,7 +52,7 @@ ln -sf $TOPGRADE_SRC/topgrade.toml $TOPGRADE_DEST
 ZELLIJ_SRC=$SRC/zellij
 ZELLIJ_DEST=$DEST/zellij
 
-mkdir -p $ZELLIJ_DEST/
+mkdir -p $ZELLIJ_DEST/theme/
 ln -sf $ZELLIJ_SRC/config.kdl -t $ZELLIJ_DEST/
 
 # bat
