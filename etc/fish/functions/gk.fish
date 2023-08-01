fd5e76fd3c6f1b5632fcc179b8b792a29fdbbd85
kaiiy <81578246+kaiiy@users.noreply.github.com>
2023-08-01 23:30:11 +0900

    feat(gk): git push skipped

--- etc/fish/functions/gk.fish
+++ etc/fish/functions/gk.fish
@@ -1,8 +1,14 @@
 function gk
+    set no_push $argv[1]
     set_color green; echo "[git add]"; set_color normal
     git add .
     if git commit -m (string join ''  "wip: " $(date --rfc-3339='seconds'))
-        set_color green; echo "[git push]"; set_color normal
-        git push
+        if test "$no_push" != "--no-push"
+            set_color green; echo "[git push]"; set_color normal
+            git push
+        else
+            set_color yellow; echo "[git push skipped]"; set_color normal
+        end
     end
 end
+
