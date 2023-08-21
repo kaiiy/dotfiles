482521f346a4f76a6a7bc3e56f1403bf30af91f5
kaiiy <81578246+kaiiy@users.noreply.github.com>
2023-08-21 21:39:04 +0900

    perf: mk brewfile

--- bin/homebrew.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-brew tap tgotwig/linux-dust
-brew install \
-  deno zellij bat dust exa fd duf procs bottom ripgrep \
-  trash-cli neovim topgrade go-task/tap/go-task rustscan \
-  gh jq latexindent
