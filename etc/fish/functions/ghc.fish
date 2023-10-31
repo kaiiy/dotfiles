4b7fab13cdd7109fe6ab8b63decccd844140a745
kaiiy <81578246+kaiiy@users.noreply.github.com>
2023-11-01 01:02:05 +0900

    feat: ghc

--- etc/fish/functions/ghc.fish
+++ etc/fish/functions/ghc.fish
@@ -1,5 +1,17 @@
 function ghc
-    set selected_repo (gh repo list --json name --jq '.[].name' -L 1000 | fzf)
+    set all_repo_names (gh repo list --json name --jq '.[].name' -L 1000)
+    set all_repo_names_str (string join "\n" $all_repo_names)
+
+    if test (count $argv) -eq 1
+        set repo_name $argv[1]
+        set matched_repo (printf $all_repo_names_str | grep $repo_name)
+        if test (count $matched_repo) -eq 1
+            gh repo clone $matched_repo
+            return
+        end
+    end
+
+    set selected_repo (printf $all_repo_names_str | fzf)
     if test -n "$selected_repo"
         gh repo clone $selected_repo
     else
