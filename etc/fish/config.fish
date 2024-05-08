ac9bf5de47445847247a7b05a35ae1295cfa9096
kaiiy <81578246+kaiiy@users.noreply.github.com>
2024-05-08 22:03:03 +0900

    Add git alias for "stash pop" command

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -89,6 +89,7 @@ if status is-interactive
     git config --global pull.rebase true
     git config --global init.defaultBranch main
     git config --global alias.st status
+    git config --global alias.pop "stash pop"
 
     git config --global diff.tool difftastic
     git config --global difftool.prompt false
