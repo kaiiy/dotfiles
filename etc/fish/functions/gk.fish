033ffb740dda862c357c7bb17b93d240c58dcd0d
k <81578246+kaiiy@users.noreply.github.com>
2024-05-18 02:48:14 +0900

    Refactor git add and commit functions in gk.fish

--- etc/fish/functions/gk.fish
+++ etc/fish/functions/gk.fish
@@ -1,14 +1,19 @@
 function gk
     set no_push $argv[1]
-    set_color green; echo "[git add]"; set_color normal
+    set_color green
+    echo "[git add]"
+    set_color normal
     git add .
-    if git commit -m (string join ''  "wip: " $(date --rfc-3339='seconds'))
-        if test "$no_push" != "--no-push"
-            set_color green; echo "[git push]"; set_color normal
+    if git commit -m (string join ''  "wip: " $(date -Iseconds))
+        if test "$no_push" != --no-push
+            set_color green
+            echo "[git push]"
+            set_color normal
             git push
         else
-            set_color yellow; echo "[git push skipped]"; set_color normal
+            set_color yellow
+            echo "[git push skipped]"
+            set_color normal
         end
     end
 end
-
